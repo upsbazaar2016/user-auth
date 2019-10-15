7dd35aae24adfd3316c5aec691e3ab16ffe9aaea
upsbazaar2016 <upsbazaar2016@gmail.com>
2019-10-15 18:53:28 +0530

    Added Comment Form and Comments List in Blog Details Component

--- src/app/features/blogs/blogs-details/blogs-details.component.ts
+++ src/app/features/blogs/blogs-details/blogs-details.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogsService } from './../blogs.service';
 import { ActivatedRoute } from '@angular/router';
-import { Blog } from 'src/app/core/interfaces';
+import { Blog, Comment } from 'src/app/core/interfaces';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { forkJoin } from 'rxjs';
+
 @Component({
   selector: 'app-blogs-details',
   templateUrl: './blogs-details.component.html',
@@ -9,12 +12,21 @@ import { Blog } from 'src/app/core/interfaces';
 })
 export class BlogsDetailsComponent implements OnInit {
   blog: Blog;
+  comments: Comment[];
   wordLimit: number;
+  commentForm: FormGroup;
 
-  constructor(private blogsService: BlogsService, private route: ActivatedRoute) { }
+  constructor(private blogsService: BlogsService, private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.wordLimit = 100;
+
+    this.commentForm = this.formBuilder.group({
+      blogId: [null, Validators.required],
+      comment: ['', [Validators.required, Validators.minLength(1)]],
+      author: ['', Validators.required],
+      date: []
+    });
 
     this.route.paramMap.subscribe(params => {
       const id = parseInt(params.get('id'), 10);
@@ -23,6 +35,35 @@ export class BlogsDetailsComponent implements OnInit {
   }
 
   getBlogDetails(id: number) {
-    this.blogsService.getBlogDetails(id).subscribe(blog => this.blog = blog);
+    const blogDetailsObservable = this.blogsService.getBlogDetails(id);
+    const blogCommentsObservable = this.blogsService.getBlogComments(id); 
+    forkJoin([blogDetailsObservable, blogCommentsObservable]).subscribe(
+      res => {
+        if (res[0]) {
+          this.blog = res[0] ? res[0] : null;
+        }
+
+        if (res[1]) {
+          this.comments = res[1] ? res[1] : [];
+        }
+      }
+    );
+  }
+
+  submitComment() {
+    this.commentForm.patchValue({
+      blogId: this.blog.id,
+      date: new Date()
+    });
+    this.blogsService.addBlogComment(this.commentForm.value).subscribe(
+      (comment: Comment) => {
+        this.comments.push(comment);
+        this.blog.comments += 1;
+        this.commentForm.reset();
+      },
+      (err) => {
+
+      }
+    );
   }
 }
