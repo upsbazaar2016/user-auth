943c628375292d4f7b31ca67ed5933b6009ad653
upsbazaar2016 <upsbazaar2016@gmail.com>
2019-10-16 17:30:01 +0530

    Added changes for Mentorship Tree in User Details Component

--- src/app/features/user/details/details.component.ts
+++ src/app/features/user/details/details.component.ts
@@ -1,3 +1,4 @@
+import { MentorshipTree } from 'src/app/core/interfaces';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
-
+  user: MentorshipTree;
+  colors: any;
   constructor() { }
 
   ngOnInit() {
+    this.user = {
+      username: 'aamanh30',
+      mentor: 'senthilnathan',
+      mentees: ['aaman.hussain', 'hussey']
+    };
+    this.colors = {
+      lines: 'red', names: 'green', circles: 'black'
+    };
   }
 
 }
--- src/app/features/user/details/details.module.ts
+++ src/app/features/user/details/details.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -9,7 +10,8 @@ import { DetailsComponent } from './details.component';
   declarations: [DetailsComponent],
   imports: [
     CommonModule,
-    DetailsRoutingModule
+    DetailsRoutingModule,
+    SharedModule
   ]
 })
 export class DetailsModule { }
